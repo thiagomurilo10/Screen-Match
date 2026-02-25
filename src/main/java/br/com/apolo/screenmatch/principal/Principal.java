3ca04d016e373c86033e24f62831e293d6f5d869
Thiago Murilo <thiagomayta10@gmail.com>
2026-02-25 15:24:27 -0300

    feat: implementa SerieRepository com derived queries e integração no menu principal

--- src/main/java/br/com/apolo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/apolo/screenmatch/principal/Principal.java
@@ -7,7 +7,6 @@ import br.com.apolo.screenmatch.model.Serie;
 import br.com.apolo.screenmatch.repository.SerieRepository;
 import br.com.apolo.screenmatch.service.ConsumoAPI;
 import br.com.apolo.screenmatch.service.ConverteDados;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -38,7 +37,9 @@ public class Principal {
             var menu = """
                     1 - Buscar séries 
                     2 - Buscar episódios 
-                    3 - Listar séries buscadas 
+                    3 - Listar séries buscadas
+                    4 - Buscar série por título
+                    5 - Buscar séries por ator
                     0 - Sair
                     """;
             System.out.println(menu);
@@ -53,6 +54,12 @@ public class Principal {
                     break;
                 case 3:
                     listarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -83,9 +90,7 @@ public class Principal {
         System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
         if (serie.isPresent()) {
             var serieEncontrada = serie.get();
@@ -117,5 +122,31 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma série pelo nome: ");
+        var nomeSerie = leitura.nextLine();
+
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if (serieBuscada.isPresent()) {
+            System.out.println("Dados da série: " + serieBuscada.get());
+        } else {
+            System.out.println("Série não encontrada!");
+        }
+    }
+
+    private void buscarSeriesPorAtor() {
+        System.out.println("Qual o nome para a busca?");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de qual valor?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
+        seriesEncontradas.forEach(s -> {
+            System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao());
+        });
     }
 }
