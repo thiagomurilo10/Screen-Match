8eb001584fd8d47aeab1bf1dcb5d49b26d37451a
Thiago Murilo <thiagomayta10@gmail.com>
2026-02-25 19:13:07 -0300

    feat: adiciona filtros avançados e consultas derivadas por categoria, avaliação e temporadas

--- src/main/java/br/com/apolo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/apolo/screenmatch/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.apolo.screenmatch.principal;
 
-import br.com.apolo.screenmatch.model.DadosSerie;
-import br.com.apolo.screenmatch.model.DadosTemporada;
-import br.com.apolo.screenmatch.model.Episodio;
-import br.com.apolo.screenmatch.model.Serie;
+import br.com.apolo.screenmatch.model.*;
 import br.com.apolo.screenmatch.repository.SerieRepository;
 import br.com.apolo.screenmatch.service.ConsumoAPI;
 import br.com.apolo.screenmatch.service.ConverteDados;
@@ -40,6 +37,10 @@ public class Principal {
                     3 - Listar séries buscadas
                     4 - Buscar série por título
                     5 - Buscar séries por ator
+                    6 - Top 5 séries
+                    7 - Buscar séries por categoria
+                    8 - Filtrar séries
+                    
                     0 - Sair
                     """;
             System.out.println(menu);
@@ -60,6 +61,15 @@ public class Principal {
                     break;
                 case 5:
                     buscarSeriesPorAtor();
+                    break;
+                case 6:
+                    buscarTop5Series();
+                    break;
+                case 7:
+                    buscarSeriesPorCategoria();
+                    break;
+                case 8:
+                    filtrarSeriesPorTemporadaEAvaliacao();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -145,8 +155,33 @@ public class Principal {
         var avaliacao = leitura.nextDouble();
         List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
         System.out.println("Séries em que " + nomeAtor + " trabalhou: ");
-        seriesEncontradas.forEach(s -> {
-            System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao());
-        });
+        seriesEncontradas.forEach(s ->
+            System.out.println(s.getTitulo() + " / avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarTop5Series() {
+        List<Serie> seriesTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        seriesTop.forEach(s ->
+            System.out.println(s.getTitulo() + " / avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriesPorCategoria() {
+        System.out.println("Deseja buscar séries de que categoria/gênero?");
+        var nomeGenero = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria " + nomeGenero);
+        seriesPorCategoria.forEach(System.out::println);
+    }
+
+    private void filtrarSeriesPorTemporadaEAvaliacao() {
+        System.out.println("Filtrar séries até quantas temporadas? ");
+        var totalTemporadas = leitura.nextInt();
+        System.out.println("Com avaliação a partir de que valor? ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> filtroSeries = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(totalTemporadas, avaliacao);
+        System.out.println("*** Séries filtradas ***");
+        filtroSeries.forEach(s ->
+                System.out.println(s.getTitulo() + " / temporadas: " + s.getTotalTemporadas() + " / avaliação: " + s.getAvaliacao()));
     }
 }
--- src/main/java/br/com/apolo/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/apolo/screenmatch/repository/SerieRepository.java
@@ -1,5 +1,6 @@
 package br.com.apolo.screenmatch.repository;
 
+import br.com.apolo.screenmatch.model.Categoria;
 import br.com.apolo.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,10 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
 
     List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
 }
